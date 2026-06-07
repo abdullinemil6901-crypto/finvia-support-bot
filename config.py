fa636e229cadabe365ab30f694b9174c632b8959
abdullinemil6901-crypto <abdullinemil6901@bk.ru>
2026-06-08 01:03:54 +0300

    Add SUPPORT_CHAT_ID

--- config.py
+++ config.py
@@ -5,6 +5,7 @@
 # 🔑 Токен от BotFather (читается из переменной окружения для Railway)
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8989311610:AAFuc6bLGjJKHzSD4cKJgJrRd32LWu0VGyk")
+SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", "5160275115"))
 
 # 👑 Список Telegram ID администраторов
 ADMIN_IDS = [8480479055]
@@ -14,4 +15,5 @@ TYPE_LABELS = {
     "cancel_payment": "Отмена платежа",
     "wrong_cvu": "Неверный CVU",
     "no_receipt": "Нет чека",
+    "other": "Другое",
 }
